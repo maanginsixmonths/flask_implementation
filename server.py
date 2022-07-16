42ce587ab03a6f6fec0b0266e63ebbc70d81bd16
Bojji <108544117+maanginsixmonths@users.noreply.github.com>
2022-07-16 15:53:39 -0400

    Add files via upload

---
+++ server.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def my_home():
+    return render_template('index.html')
+
+@app.route("/about.html")
+def about():
+    return render_template('about.html')
+
+@app.route("/works.html")
+def work():
+    return render_template('works.html')
